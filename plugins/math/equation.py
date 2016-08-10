19856edee5bd52e3ecbf3b4e0759237a4acb9fd6
William Granados <william.granados@wgma00.me>
2016-08-09 20:21:29 -0400

    fixed security flaw and added some error checking

--- plugins/math/equation.py
+++ plugins/math/equation.py
@@ -24,15 +24,36 @@ import subprocess
 
 MATH_CONST = {"pi":"π"}
 
+def sanatize_input(user_input):
+    """ Removes nasty stuff that could bork the system."""
+    user_input = user_input.replace('$', '')
+    user_input = user_input.replace('|', '')
+    user_input = user_input.replace('>', '')
+    user_input = user_input.replace("'", '')
+    user_input = user_input.replace('"', '')
+    user_input = user_input.replace('rm', '')
+    user_input = user_input.replace('quit', '')
+    user_input = user_input.replace('exit', '')
+    return user_input
+
 def solve(user_input, x=""):
     """ Solves a mathematical expresion using the gnome calculator software."""
+    # sanatize input to avoid security errors
+    user_input = sanatize_input(user_input)
+    x = sanatize_input(user_input)
     for const in MATH_CONST:
         user_input = user_input.replace(const, MATH_CONST[const])
         x = x.replace(const, MATH_CONST[const])
     user_input = user_input.replace("x", x)
     out = subprocess.check_output("echo '{uinput}' | gcalccmd".format(uinput=user_input), shell=True)
     ret = out.decode("utf-8")
-    return ret[ret.index(">")+1:ret[ret.index(">")+1:].index(">")].strip()                                          
+    print(ret)
+    if(ret == None or ret.count('>') != 2):
+        return "invalid"
+    if(ret.count('>') == 2 and ret[ret.index(">")+1:ret[ret.index(">")+1:].index(">")].strip().replace(' ','') == ''):
+        return "invalid"
+    else:
+        return ret[ret.index(">")+1:ret[ret.index(">")+1:].index(">")].strip()                                          
 
 if __name__ == "__main__":
     print(solve("1+2"))
