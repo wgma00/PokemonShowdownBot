e546cf8292a8664b63c328c8456b131fc0198b4d
William Granados <me@wgma00.me>
2016-12-23 10:20:25 -0500

    added some prelimnary files needed for creating unit tests using the pytest framework

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DNF_CMD=$(which dnf)
+APT_GET_CMD=$(which apt-get)
+PIP_CMD=$(which pip)
+# install the relevant pip manager if it's not installed
+if [[ -z ${PIP_CMD} ]]; then
+    wget https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py
+    echo "pip installed for python3"
+else
+    echo "pip already installed"
+fi
+echo "Installing pip dependacies to run PokemonShowdownBot"
+pip install -r requirements.txt
+# install the required libraries for latex
+# related libraries and calculator related libraries
+echo "Installing relevant Linux modules"
+if [[ ! -z ${DNF_CMD} ]]; then
+    dnf install gnome-calculator
+    dnf install texlive
+    dnf install poppler-utils
+elif [[ ! -z ${APT_GET_CMD} ]]; then
+    apt-get install texlive-full
+    apt-get install poppler-utls
+    apt-get install gnome-calculator
+fi
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='PokemonShowdownBot',
+      version='1.0',
+      description='PokemonShowdown chatbot',
+      long_description=readme(),
+      classifiers=[
+        'License :: OSI Approved :: GPLv3',
+        'Programming Language :: Python :: 3.4+',
+        'Topic :: Chat Bot',
+      ],
+      keywords='chatbot latex chatgames ',
+      url='http://github.com/wgma00/PokemonShowdownBot',
+      author='William Granados',
+      author_email='me@wgma00.me',
+      license='GPLv3',
+      packages=['pokemonshowdownbot'],
+      platforms=['deb', 'rpm'],
+      install_requires=[
+          'PyYAML',
+          'websocket',
+          'requests',
+          'simplejson', 
+          'websocket-client', 
+          'cleverbot', 
+          'pylatex', 
+          'pyimgur', 
+          'markdown',
+          'pytest'
+      ],
+      include_package_data=True,
+      zip_safe=False)
