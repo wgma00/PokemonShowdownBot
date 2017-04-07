4d3e38af911b78a1429efbd2f1dc3730d923de86
William Granados <me@wgma00.me>
2017-04-07 06:14:02 -0400

    updated build scripts

--- install.sh
+++ install.sh
@@ -1,28 +1,56 @@
 #!/bin/bash
-DNF_CMD=$(which dnf)
-APT_GET_CMD=$(which apt-get)
-PIP_CMD=$(which pip)
+
+
+install_dep_apt() {
+    apt-get install texlive-full
+    apt-get install poppler-utils
+    # might not be installed on some systems with poppler-utils
+    apt-get install pnmtopng
+    # gnome-calculator package was formally known as gcalctool on earlier
+    # versions like ubuntu 12.04
+    if [[ -z `apt-cache search gnome-calculator` ]]; then
+        apt-get install gcalctool
+    else
+        apt-get install gnome-calculator
+    fi
+}
+
+install_dep_dnf() {
+    dnf install gnome-calculator
+    dnf install texlive
+    dnf install poppler-utils
+}
+
+
+DNF_CMD=`which dnf`
+APT_GET_CMD=`which apt-get`
+PIP_CMD=`which pip`
+
+# prompting for sudo 
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+
 # install the relevant pip manager if it's not installed
-if [[ -z ${PIP_CMD} ]]; then
+if [[ -z PIP_CMD ]]; then
     wget https://bootstrap.pypa.io/get-pip.py
     python3 get-pip.py
     echo "pip installed for python3"
 else
     echo "pip already installed"
 fi
+
 echo "Installing pip dependacies to run PokemonShowdownBot"
+
 pip install -r requirements.txt
+
 # install the required libraries for latex
 # related libraries and calculator related libraries
 echo "Installing relevant Linux modules"
-if [[ ! -z ${DNF_CMD} ]]; then
-    dnf install gnome-calculator
-    dnf install texlive
-    dnf install poppler-utils
-elif [[ ! -z ${APT_GET_CMD} ]]; then
-    apt-get install texlive-full
-    apt-get install poppler-utils
-    # might not be installed on some systems with poppler-utils
-    apt-get install pnmtopng
-    apt-get install gcalctool
+if [[ ! -z $DNF_CMD ]]; then
+    install_dep_dnf
+elif [[ ! -z $APT_GET_CMD ]]; then
+    install_dep_apt
 fi
--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python3 -m pytest test
+if [[ ! -z `pip3 list | grep pytest` ]]; then
+	python3 -m pytest test
+else
+	echo "Pytest is not installed for python3. Install by executing install.sh"
+fi
