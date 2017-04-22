6119ed7b91060a8b77e71b3806ba38887b65d53a
William Granados <me@wgma00.me>
2017-04-22 17:04:17 -0400

    added some support for piping calc command to ubuntu bash port, untested

--- plugins/math/equation.py
+++ plugins/math/equation.py
@@ -18,6 +18,9 @@
 import subprocess
 import threading
 import queue
+import platform
+import tempfile
+import os
 
 
 def MATH_CONST():
@@ -37,9 +40,9 @@ def sanitize_input(user_input):
     return user_input
 
 
-def solve(user_input, x=""):
-    """Solves a mathematical expression using the gnome calculator software."""
-    # sanitize input to avoid security errors
+
+def solve_on_standard_posix(user_input, x):
+    """Solves the equation on a normal unix or unix like operating system"""
     user_input = sanitize_input(user_input)
     x = sanitize_input(x)
     for const in MATH_CONST():
@@ -56,5 +59,51 @@ def solve(user_input, x=""):
     if ret.count('>') == 2 and ret[ret.index(">")+1:ret[ret.index(">")+1:].index(">")].strip().replace(' ', '') == '':
         return "invalid"
     else:
-        return ret[ret.index(">")+1:ret[ret.index(">")+1:].index(">")].strip()                                          
+        return ret[ret.index(">")+1:ret[ret.index(">")+1:].index(">")].strip()
 
+
+def solve_on_windows_running_ubuntu_bash(user_input, x):
+    user_input = sanitize_input(user_input)
+    x = sanitize_input(x)
+    for const in MATH_CONST():
+        user_input = user_input.replace(const, MATH_CONST()[const])
+        x = x.replace(const, MATH_CONST()[const])
+    user_input = user_input.replace("x", x)
+    out = subprocess.check_output("echo '{uinput}' | gcalccmd".format(uinput=user_input), shell=True,
+                                  executable="C:\\Windows\\System32\\bash.exe")
+    ret = out.decode("utf-8")
+    print(ret)
+    if ret or ret.count('>') != 2:
+        return "invalid"
+    if(ret.count('>') == 2
+       and ret[ret.index(">") + 1:ret[ret.index(">") + 1:].index(">")].strip().replace(' ', '') == ''):
+        return "invalid"
+    else:
+        return ret[ret.index(">") + 1:ret[ret.index(">") + 1:].index(">")].strip()
+
+
+def solve(user_input, x=""):
+    """Solves a mathematical expresion using the gnome calculator software."""
+    # sanatize input to avoid security errors
+    if os.name != 'nt':
+        return solve_on_standard_posix(user_input, x)
+    else:
+        return solve_on_windows_running_ubuntu_bash(user_input, x)
+
+
+def wintolin(path):
+    path = os.path.abspath(path)
+    if path[1:2] == ':':
+        drive = path[:1].lower()
+        return '/mnt/' + drive + path[2:].replace('\\', '/')
+
+if __name__ == "__main__":
+    is32bit = (platform.architecture()[0] == '32bit')
+    system32 = os.path.join(os.environ['SystemRoot'],
+                            'SysNative' if is32bit else 'System32')
+    bash = os.path.join(system32, 'cmd.exe')
+
+    #subprocess.check_call('"%s" -c "echo \'hello world\'"' % bash)
+    out = subprocess.check_output("bash -c pwd", shell=True, executable=bash, stderr=subprocess.PIPE)
+    ret = out.decode("utf-8")
+    print(ret)
